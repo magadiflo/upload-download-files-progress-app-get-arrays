f602b27d0c02de8f6743f422165032a8e61c0b83
Martín <magadiflo@gmail.com>
2022-07-13 17:37:56 -0500

    Using the Angular Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+import { FileService } from './file.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+  constructor(private fileService: FileService) { }
+
+  onUploadFiles(files: File[]): void {
+    const formData = new FormData();
+    files.forEach(file => formData.append('files', file, file.name));
+    this.fileService.upload(formData)
+      .subscribe({
+        next: (event: HttpEvent<string[]>) => {
+          console.log(event);
+          this.reportPorgress(event);
+        },
+        error: (error: HttpErrorResponse) => console.log(error)
+      });
+  }
+
+  onDownloadFiles(filename: string): void {
+    this.fileService.download(filename)
+      .subscribe({
+        next: (event: HttpEvent<Blob>) => {
+          console.log(event);
+          this.reportPorgress(event);
+        },
+        error: (error: HttpErrorResponse) => console.log(error)
+      });
+  }
+
+  private reportPorgress(httpEvent: HttpEvent<string[] | Blob>) {
+    throw new Error('Method not implemented.');
+  }
+
 }
